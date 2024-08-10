0cead389fbf63691da757b50e235b86b33d40d3a
AGuwf <kesantiw@gmail.com>
2024-08-10 23:09:15 +0700

    update code

--- src/components/modals/CreateStoryModal.tsx
+++ src/components/modals/CreateStoryModal.tsx
@@ -11,27 +11,27 @@ import {
 	ModalHeader,
 } from "@nextui-org/modal";
 import { Button, Input, Textarea } from "@nextui-org/react";
-import type { GetProp, PopconfirmProps, UploadFile, UploadProps } from "antd";
-import { Image, Popconfirm, Upload, message } from "antd";
-import { PlusSignIcon } from "hugeicons-react";
+import type { PopconfirmProps, UploadFile } from "antd";
+import { Image, Popconfirm, message } from "antd";
+// import { PlusSignIcon } from "hugeicons-react";
 import { useEffect, useState } from "react";
 
-const uploadButton = (
-	<button className="border-0 bg-none" type="button">
-		<PlusSignIcon className="mx-auto" />
-		<div className="mt-2">Upload</div>
-	</button>
-);
+// const uploadButton = (
+// 	<button className="border-0 bg-none" type="button">
+// 		<PlusSignIcon className="mx-auto" />
+// 		<div className="mt-2">Upload</div>
+// 	</button>
+// );
 
-type FileType = Parameters<GetProp<UploadProps, "beforeUpload">>[0];
+// type FileType = Parameters<GetProp<UploadProps, "beforeUpload">>[0];
 
-const getBase64 = (file: FileType): Promise<string> =>
-	new Promise((resolve, reject) => {
-		const reader = new FileReader();
-		reader.readAsDataURL(file);
-		reader.onload = () => resolve(reader.result as string);
-		reader.onerror = (error) => reject(error);
-	});
+// const getBase64 = (file: FileType): Promise<string> =>
+// 	new Promise((resolve, reject) => {
+// 		const reader = new FileReader();
+// 		reader.readAsDataURL(file);
+// 		reader.onload = () => resolve(reader.result as string);
+// 		reader.onerror = (error) => reject(error);
+// 	});
 
 interface StoryData {
 	title: string;
@@ -87,17 +87,17 @@ export default function CreateStoryModal({
 		}
 	}, [selectedStory]);
 
-	const handlePreview = async (file: UploadFile) => {
-		if (!file.url && !file.preview) {
-			file.preview = await getBase64(file.originFileObj as FileType);
-		}
+	// const handlePreview = async (file: UploadFile) => {
+	// 	if (!file.url && !file.preview) {
+	// 		file.preview = await getBase64(file.originFileObj as FileType);
+	// 	}
 
-		setPreviewImage(file.url || (file.preview as string));
-		setPreviewOpen(true);
-	};
+	// 	setPreviewImage(file.url || (file.preview as string));
+	// 	setPreviewOpen(true);
+	// };
 
-	const handleChange: UploadProps["onChange"] = ({ fileList: newFileList }) =>
-		setFileList(newFileList);
+	// const handleChange: UploadProps["onChange"] = ({ fileList: newFileList }) =>
+	// 	setFileList(newFileList);
 
 	const onInputChange = (e: any) => {
 		const { name, value } = e.target;

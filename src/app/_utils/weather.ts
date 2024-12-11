78a1cba1789b2fa99c7344035169ea5a23476c78
AGuwf <kesantiw@gmail.com>
2024-12-11 14:56:17 +0700

    feature: redesign home page and detail, update func heart and comment

--- src/app/_utils/weather.ts
+++ src/app/_utils/weather.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { env } from "@/env";
 
 export const getWeather = async () => {
-  const url = `http://api.weatherapi.com/v1/current.json?key=${env.NEXT_PUBLIC_WEATHER_API_KEY}&q=Hanoi&aqi=no`;
+  const url = `https://api.weatherapi.com/v1/current.json?key=${env.NEXT_PUBLIC_WEATHER_API_KEY}&q=Hanoi&aqi=no`;
   const response = await axios.get(url);
   return response.data;
 };
